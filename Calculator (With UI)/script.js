8425d90cd811280db6c918033651fba9b22c9d98
Nagapuri Swathi <83587716+NagapuriSwathi@users.noreply.github.com>
2025-09-04 08:06:29 +0530

    Add files via upload

--- Calculator (With UI)/script.js
+++ Calculator (With UI)/script.js
@@ -4,27 +4,35 @@ const calculatorInputElements = document.getElementsByClassName("calculator-inpu
 let calculatorInputs = Array.from(calculatorInputElements)
 
 calculatorInputs.forEach((calculatorInput) => {
-    // console.log(calculatorInput)
-    calculatorInput.addEventListener("click", (event) => {
+    try{
+        calculatorInput.addEventListener("click", (event) => {
+            calculatorDisplayOutputElement.style.backgroundColor = "#3D5F07";
+            calculatorDisplayOutputElement.style.color = "#FFFFFF";
 
-        if(event.target.innerText === "AC"){
-            calculatorDisplayOutputElement.value = "";
-        }
-        else if(event.target.innerText === "DEL"){
-            let calculatorOutput = calculatorDisplayOutputElement.value;
-            calculatorDisplayOutputElement.value = calculatorOutput.substring(0, calculatorOutput.length-1);
-        }
-        else if(event.target.innerText === "="){
-            if(calculatorDisplayOutputElement.value){
-                calculatorDisplayOutputElement.value = eval(calculatorDisplayOutputElement.value);
+            if(event.target.innerText === "AC"){
+                calculatorDisplayOutputElement.value = "";
+                calculatorDisplayOutputElement.style.backgroundColor = "";
+            }
+            else if(event.target.innerText === "DEL"){
+                let calculatorOutput = calculatorDisplayOutputElement.value;
+                calculatorDisplayOutputElement.value = calculatorOutput.substring(0, calculatorOutput.length-1);
+            }
+            else if(event.target.innerText === "="){
+                if(calculatorDisplayOutputElement.value){
+                    calculatorDisplayOutputElement.value = eval(calculatorDisplayOutputElement.value);
+                }
+                else{
+                    calculatorDisplayOutputElement.value = "";
+                }
             }
             else{
-                calculatorDisplayOutputElement.value = ""
+                calculatorDisplayOutputElement.value += event.target.innerText;
             }
-        }
-        else{
-            calculatorDisplayOutputElement.value += event.target.innerText;
-        }
-    });
+        });
+    }
+    catch (error) {
+        console.log(error);
+        calculatorDisplayOutputElement.value = " ";
+    }
 });
 
